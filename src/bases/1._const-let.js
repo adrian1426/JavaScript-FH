e035861dc4cf04e6ce7719f1d8afe297e818a1ab
Adrian1426 <adrianhh1426@gmail.com>
2021-04-25 20:22:47 -0500

    const - let

---
+++ src/bases/1._const-let.js
@@ -0,0 +1,11 @@
+let nombre = 'Adrian';
+
+console.log("nombre: ", nombre);
+
+if (true) {
+  let nombre = "Adrian if";
+
+  console.log("nombreScope: ", nombre);
+}
+
+console.log("nombreFinal: ", nombre);
