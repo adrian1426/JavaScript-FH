8ae8f7df56242e6e10d0e53e5b25f45684924808
Adrian1426 <adrianhh1426@gmail.com>
2021-05-15 14:26:52 -0500

    arreglos

---
+++ src/bases/4.-arreglos.js
@@ -0,0 +1,8 @@
+// const arreglo = new Array();
+const arreglo = [1, 2, 3, 4];
+const arreglo2 = [...arreglo, 5];
+const arreglo3 = arreglo.map(x => x * 2);
+
+console.log('arr1: ', arreglo);
+console.log('arr2: ', arreglo2);
+console.log('arr3: ', arreglo3);
